3fb14520d134610ffca74e0ea3e1b8b015b32da0
Kevin Heidema <heidemakevin@gmail.com>
2023-09-09 20:07:03 -0700

    React site part 1 complete
    Add the campsites and header

--- WebDev/3-React/nucampsite/src/App.js
+++ WebDev/3-React/nucampsite/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
-import NucampLogo from './app/assets/img/logo.png';
-
-// We can import specific components from ReactStrap
-import {Container, Navbar, NavbarBrand } from 'reactstrap';
+import Header from './components/Header.js';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='Nucamp Logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      I'm ready for the workshop!
+      <Header></Header>
+      <CampsitesList></CampsitesList>
+      <Footer></Footer>
     </div>
   );
 }
