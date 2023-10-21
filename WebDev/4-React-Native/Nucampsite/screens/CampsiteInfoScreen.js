c97fdafe2497fa3cc39640cc540d6e92a02b93cf
Kevin Heidema <heidemakevin@gmail.com>
2023-10-21 13:46:56 -0700

    Added selected campsite state in React Native app

---
+++ WebDev/4-React-Native/Nucampsite/screens/CampsiteInfoScreen.js
@@ -0,0 +1,11 @@
+import RenderCampsite from '../features/campsites/RenderCampsite';
+
+const CampsiteInfoScreen = (props) => {
+
+    return (
+        <RenderCampsite campsite={props.campsite} />
+    )
+
+};
+
+export default CampsiteInfoScreen;
