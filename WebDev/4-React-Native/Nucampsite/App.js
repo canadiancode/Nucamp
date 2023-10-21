ec451338fc0b46b973ad45be7cbe8fb3a7b20f7d
Kevin Heidema <heidemakevin@gmail.com>
2023-10-21 13:30:00 -0700

    React Native Init

---
+++ WebDev/4-React-Native/Nucampsite/App.js
@@ -0,0 +1,10 @@
+import Main from './screens/MainComponent';
+
+// This will stop the yellow screen on my phone:
+// console.disableYellowBox = true;
+
+export default function App() {
+  return (
+    <Main />
+  );
+};
